d61e14dd9ad251ea90c1ffbf6c478c6e4fdb0fae
Atieeh <134216242+Atieeh@users.noreply.github.com>
2023-05-21 17:44:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function openNewPage() {
+  window.open(
+    "https://www.youtube.com/channel/UCMsLYg6Cm1VHH_To8mt5qfQ",
+    "_blank"
+  );
+}
+
+function openNewPage2() {
+  window.open("https://www.instagram.com", "_blank");
+}
+
+function openNewPage3() {
+  window.open(
+    "https://www.amazon.com/dp/B078PJG2X6/ref=cm_sw_r_awdo_VRC7RN5MCAGM31HAT4C7",
+    "_blank"
+  );
+}
